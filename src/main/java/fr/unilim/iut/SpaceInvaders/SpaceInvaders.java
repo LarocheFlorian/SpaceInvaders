0b6f67b24c19c5e2967d8fc583e11afce83c856c
laroche28 <florian.laroche1@etu.unilim.fr>
2019-04-29 09:40:33 +0200

    Se déplacer vers la droite

--- src/main/java/fr/unilim/iut/SpaceInvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/SpaceInvaders/SpaceInvaders.java
@@ -21,9 +21,7 @@ public class SpaceInvaders {
 		return recupererEspaceJeuDansChaineASCII();
 	}
 
-	/**
-	 * @return
-	 */
+
 	public String recupererEspaceJeuDansChaineASCII() {
 		StringBuilder espaceDeJeu = new StringBuilder();
 		for (int y = 0; y < hauteur; y++) {
@@ -35,6 +33,7 @@ public class SpaceInvaders {
 		return espaceDeJeu.toString();
 	}
 
+	
 	private char recupererMarqueDeLaPosition(int x, int y) {
 		char marque;
 		if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
@@ -44,16 +43,18 @@ public class SpaceInvaders {
 		return marque;
 	}
 
+	
 	private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
 		return this.aUnVaisseau() && vaisseau.occupeLaPosition(x,y);
 	}
 
+	
 	private boolean aUnVaisseau() {
 		return vaisseau!=null;
 	}
     
     
-public void positionnerUnNouveauVaisseau(int x, int y) {
+	public void positionnerUnNouveauVaisseau(int x, int y) {
 		
 		if (!estDansEspaceDeJeu(x, y))
 			throw new HorsEspaceJeuException("Vous êtes en dehors de l'espace jeu");
@@ -62,12 +63,15 @@ public void positionnerUnNouveauVaisseau(int x, int y) {
 
 	}
 
-/**
- * @param x
- * @param y
- * @return
- */
-private boolean estDansEspaceDeJeu(int x, int y) {
-	return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
-}
+	
+	private boolean estDansEspaceDeJeu(int x, int y) {
+		return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
+	}
+	
+	
+	public void deplacerVaisseauVersLaDroite() {
+		   vaisseau.seDeplacerVersLaDroite();
+	    }
+	
+	
 }
