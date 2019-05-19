b34db825ced3dd77e2c56d330fbcf5e4cb4f16f2
laroche28 <florian.laroche1@etu.unilim.fr>
2019-05-19 19:53:35 +0200

    changement commandes touches

--- src/test/java/fr/unilim/iut/SpaceInvaders/VaisseauTest.java
+++ src/test/java/fr/unilim/iut/SpaceInvaders/VaisseauTest.java
@@ -12,7 +12,7 @@ import fr.unilim.iut.spaceinvaders.utils.MissileException;
 
 public class VaisseauTest {
 	
-	@Test(expected = MissileException.class)
+	@Test(expected = MissileException.class)	
 	public void test_LongueurMissileSuperieureALongueurVaisseau_UneExceptionEstLevee() throws Exception {
 		Vaisseau vaisseau = new Vaisseau(new Dimension(5,2),new Position(5,9), 1);
 		vaisseau.tirerUnMissile(new Dimension(7,2),1);
